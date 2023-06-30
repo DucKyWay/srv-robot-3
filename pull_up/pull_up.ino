371d4fed196117cb77cdd9d110d51ac0facfc657
DucKyWay <niji2549@gmail.com>
2023-06-30 20:49:16 +0700

    fix what bug

--- pull_up/pull_up.ino
+++ pull_up/pull_up.ino
@@ -6,9 +6,9 @@ char wtfSerial = '0';
 
 void setup() {
 
-    pinMode(led_1, INPUT);
-    pinMode(led_2, INPUT);
-    pinMode(led_3, INPUT);
+    pinMode(led_1, OUTPUT);
+    pinMode(led_2, OUTPUT);
+    pinMode(led_3, OUTPUT);
 
     Serial.begin(9600);
     Serial.println("Program is Start.");
@@ -26,21 +26,21 @@ void loop() {
         Serial.println(wtfSerial);
     }
     if(wtfSerial == '0') {
-    	  digitalWrite(led_1, LOW);
+    	digitalWrite(led_1, LOW);
         digitalWrite(led_2, LOW);
         digitalWrite(led_3, LOW);
         Serial.println("All LED is off");
     }
     if(wtfSerial == '1') {
-    	  digitalWrite(led_1, HIGH);
+    	digitalWrite(led_1, HIGH);
         Serial.println("LED 1 is on");
     }
       if(wtfSerial == '2') {
-    	  digitalWrite(led_2, HIGH);
+    	digitalWrite(led_2, HIGH);
         Serial.println("LED 2 is on");
     }
       if(wtfSerial == '3') {
-    	  digitalWrite(led_3, HIGH);
+    	digitalWrite(led_3, HIGH);
         Serial.println("LED 3 is on");
     }
 } 
