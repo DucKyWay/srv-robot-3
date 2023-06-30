f18fe9dc6005b55c0932c1baa282e573b4fa321a
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-06-30 19:41:43 +0700

    update pull, lcd

--- lcd/lcd.ino
+++ lcd/lcd.ino
@@ -1,3 +1,7 @@
+#include <Adafruit_LiquidCrystal.h>
+
+Adafruit_LiquidCrystal lcd(0x20, 16, 2);
+
 int led_1 = 5;
 int led_2 = 4;
 int led_3 = 3;
@@ -5,7 +9,7 @@ int led_3 = 3;
 char wtfSerial = '0';
 
 void setup() {
-
+  
     pinMode(led_1, INPUT);
     pinMode(led_2, INPUT);
     pinMode(led_3, INPUT);
@@ -16,6 +20,11 @@ void setup() {
     digitalWrite(led_1, LOW);
     digitalWrite(led_2, LOW);
     digitalWrite(led_3, LOW);
+  
+    lcd.begin(16, 2);
+    lcd.setBacklight(LOW);
+    delay(1000); 
+    lcd.setBacklight(HIGH);
 }
 
 void loop() {
@@ -26,21 +35,33 @@ void loop() {
         Serial.println(wtfSerial);
     }
     if(wtfSerial == '0') {
-    	  digitalWrite(led_1, LOW);
+    	digitalWrite(led_1, LOW);
         digitalWrite(led_2, LOW);
         digitalWrite(led_3, LOW);
         Serial.println("All LED is off");
     }
     if(wtfSerial == '1') {
-    	  digitalWrite(led_1, HIGH);
+    	digitalWrite(led_1, HIGH);
         Serial.println("LED 1 is on");
     }
       if(wtfSerial == '2') {
-    	  digitalWrite(led_2, HIGH);
+    	digitalWrite(led_2, HIGH);
         Serial.println("LED 2 is on");
     }
       if(wtfSerial == '3') {
-    	  digitalWrite(led_3, HIGH);
+    	digitalWrite(led_3, HIGH);
         Serial.println("LED 3 is on");
     }
-} 
+
+    lcd.setBacklight(LOW);
+    delay(1000);
+    lcd.setBacklight(HIGH);
+    lcd.setCursor(0,0);
+    lcd.print("Hello, world!");
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print("I am an LCD");
+    delay(2000);
+    lcd.clear();
+}   
